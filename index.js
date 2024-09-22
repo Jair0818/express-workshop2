b8e3614510ab12881907dfec9b9c530a409b2dff
Jair0818 <162615487+Jair0818@users.noreply.github.com>
2024-09-21 20:26:26 -0600

    added creacion de readme y instalacion de nodemon

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express= require('express');
 const app = express();
-app.get("/", (req,res,next)=>{
-    res.send("Hola Mundo");
+app.get("/", (req, res, next)=>{
+    res.status(200);
+    res.send("Bienvenido al servidor");
 });
-app.listen(3000,()=>{
-    console.log("todo funciona");
+app.get("/:name", (req,res,next)=>{
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola," + req.params.name);
+});
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Sever is runing...");
 });
