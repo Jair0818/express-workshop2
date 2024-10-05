8d46755afa358d180c0df9fdb608597136454fe1
Jair0818 <162615487+Jair0818@users.noreply.github.com>
2024-10-05 12:58:55 -0600

    added user

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express= require('express');
 const app = express();
 //Routes
 const pokemon=require('./routes/pokemon');
+const user=require('./routes/user');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.get("/", (req, res, next)=>{
     return res.status(200).json({code:1,message:"Bienvenidos al Pokedex"});
 });
+app.use("/user",user);
 app.use("/pokemon",pokemon);
 app.use((req,res,next)=>{
     return res.status(404).json({code:404,message:"URL no encontrada"});
